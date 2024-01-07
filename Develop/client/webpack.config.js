201e3bb18cfeff5a1763cb0bc5c56584b6ac23fb
krosengr4 <rosenkev4@gmail.com>
2024-01-07 01:55:40 -0600

    Added plugins, service worker, css and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index/html',
+        title: 'PWA text-editor'
+      }),
+      // Inject our custom service worker
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'PWA text-editor',
+        short_name: 'text-editor',
+        description: 'Edit your texts',
+        background_color: '#7eb4e2',
+        theme_color: '#73b4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ]
+      })
       
     ],
-
+    // CSS loaders
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel loaders (ES6).
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
